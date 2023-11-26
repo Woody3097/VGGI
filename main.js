a81b0e389d3dab3001626124f25ef8127681f2eb
NicolayDanch <nicolay.danch@qroot.app>
2023-11-26 22:47:01 +0200

    pa-1: initial setup

--- main.js
+++ main.js
@@ -5,6 +5,29 @@ let surface;                    // A surface model
 let shProgram;                  // A shader program
 let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
 
+// Vertex shader
+const vertexShaderSource = `
+attribute vec3 vertex;
+uniform mat4 ModelViewProjectionMatrix;
+
+void main() {
+    gl_Position = ModelViewProjectionMatrix * vec4(vertex,1.0);
+}`;
+
+
+// Fragment shader
+const fragmentShaderSource = `
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+   precision highp float;
+#else
+   precision mediump float;
+#endif
+
+uniform vec4 color;
+void main() {
+    gl_FragColor = color;
+}`;
+
 function deg2rad(angle) {
     return angle * Math.PI / 180;
 }
